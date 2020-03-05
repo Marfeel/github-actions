8880794331d34b14cad93a923d65484e47453f90
Raquel Yuste <raquel.yuste@marfeel.com>
2020-03-05 16:11:28 +0100

    fix(adservers):set git credentials before publish playground

--- utils/index.js
+++ utils/index.js
@@ -2,22 +2,34 @@ const { execSync } = require('child_process');
 
 function getInput(name, options) {
     const val = process.env[`INPUT_${name.replace(/ /g, '_').toUpperCase()}`] || '';
+
     if (options && options.required && !val) {
         throw new Error(`Input required and not supplied: ${name}`);
     }
+
     return val.trim();
 }
-exports.getInput = getInput;
 
 function setFailed(message) {
     process.exitCode = 1;
     console.error(message);
 }
-exports.setFailed = setFailed;
 
-function execStep(command, message) {
+function execStep(commands, message) {
     console.info(message);
-    execSync(command, {stdio: 'inherit'});
+    const execCommand = (command) => execSync(command, {stdio: 'inherit'});
+    
+    if (commands.isArray) {
+        commands.forEach((command) => execCommand(command));
+    } else {
+        execCommand(command);
+    }    
+    
     console.info('\n');
 }
-exports.execStep = execStep;
+
+exports = {
+    getInput,
+    setFailed,
+    execStep
+};
