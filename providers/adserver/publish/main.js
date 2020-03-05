a9bfcb791b57011ad3248b32885c1969ff28d5d6
Raquel Yuste <raquel.yuste@marfeel.com>
2020-03-05 18:08:21 +0100

    commented plauground publich system

--- providers/adserver/publish/main.js
+++ providers/adserver/publish/main.js
@@ -5,24 +5,24 @@ try {
     const awsKey = utils.getInput('aws-key');
     const awsSecret = utils.getInput('aws-secret');
 
-    const userEmail = 'tech@marfeel.com';
-    const userName = 'Github Action';
+    // const userEmail = 'tech@marfeel.com';
+    // const userName = 'Github Action';
 
-    utils.execStep(
-        'npm publish',
-        '🚀Publishing adserver 📦Package📦...'
-    );
-    utils.execStep(
-        [
-            `git config --local user.email '${userEmail}'`,
-            `git config --local user.name '${userName}'`,
-            `npx adserver-providers playground:publish --gh-token ${ghToken}`
-        ],
-        '🚀Publishing adserver 🕹Playground🕹...'
-    );
+    // utils.execStep(
+    //     [
+    //         `git config --local user.email '${userEmail}'`,
+    //         `git config --local user.name '${userName}'`,
+    //         `npx adserver-providers playground:publish --gh-token ${ghToken}`
+    //     ],
+    //     '🚀Publishing adserver 🕹Playground🕹...'
+    // );
     utils.execStep(
         `npx adserver-providers publish:s3 --aws-key ${awsKey} --aws-secret ${awsSecret}`,
         '🚀Publishing adserver { schema }...'
+    );
+    utils.execStep(
+        'npm publish',
+        '🚀Publishing adserver 📦Package📦...'
     );
 } catch (error) {
     utils.setFailed(error.message);
