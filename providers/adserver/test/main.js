459f8606a1aefd837545f502b49422d43c758571
alexmullermrf <alex.muller@marfeel.com>
2020-04-15 13:47:38 +0200

    Move version check to test step

--- providers/adserver/test/main.js
+++ providers/adserver/test/main.js
@@ -11,8 +11,20 @@ try{
     );
     utils.execStep(
         'npx adserver-providers lint',
-        '🔦Cheking lint...'
+        '🔦Checking lint...'
     );
+
+    const { name, version } = require(`${process.cwd()}/package.json`);
+    
+    const result = utils.execSync(
+        `npm view ${name}@${version}`, 
+        '🧐 Checking if version already exists'
+    );
+
+    if (result.length !== 0) {
+        throw `⚠️ Version ${version} already exists. Remember to bump the version:\nnpm version [<newversion> | major | minor | patch | premajor | preminor | prepatch | prerelease]`;
+    }
+
 } catch (error) {
     utils.setFailed(error.message);
 }
