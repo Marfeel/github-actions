055a7e33e173bb92646547fad9d270adc47a578a
Oscar Dominguez <dominguez.celada@gmail.com>
2020-10-06 10:40:47 +0200

    fix(initialize): remove unused config setup on initialize step

--- common/actions/initialize/main.js
+++ common/actions/initialize/main.js
@@ -1,15 +1,9 @@
-const { getInput, execStep, setFailed } = require('../../../utils');
+const {
+    execStep,
+    setFailed
+} = require('../../../utils');
 
-try{
-    const npmNexusAuth = getInput('nexus-token');
-
-    execStep(
-        [
-            `npm config set //repositories-proxy.mrf.io/nexus/repository/npm-internal/:_authToken ${npmNexusAuth}`,
-            'npm config set strict-ssl false'
-        ],
-        '🔐Config access to marfeel packages...'
-    );
+try {
     execStep(
         'npm ci',
         '📦Installing packages...'
