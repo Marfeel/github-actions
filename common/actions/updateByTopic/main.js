460ada130607ac663812ce8eba48b94d69ad75ef
Marc <marc.garcia@marfeel.com>
2021-12-17 11:09:14 -0500

    fix action

--- common/actions/updateByTopic/main.js
+++ common/actions/updateByTopic/main.js
@@ -9,7 +9,7 @@ try {
 
 	execStep(
 		[
-			`curl -X POST https://5fh0v4yca1.execute-api.eu-west-1.amazonaws.com/prod/update-manager-topic -H "Accept:application/json" --data-raw "{\"topic\":\"${topic}\"}"`
+			`curl --request POST https://5fh0v4yca1.execute-api.eu-west-1.amazonaws.com/prod/update-manager-topi --data-raw "{\"topic\":\"${topic}\"}"`
 		],
 		`Triggering update manager with topic: ${topic}...`
 	)
