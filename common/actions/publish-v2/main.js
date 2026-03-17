61525048dd0ea6f52dbd09889850ef81150d25d5
Jonay Rodríguez <36264905+jonayrodriguezmrf@users.noreply.github.com>
2026-03-17 15:25:06 +0100

    feat: publish with node 24 (#70)
    * feat: publish with node 24
    
    * use --tag for npm 11

---
+++ common/actions/publish-v2/main.js
@@ -0,0 +1,43 @@
+const {
+	getInput,
+	execStep,
+	setFailed
+} = require('../../../utils');
+
+try {
+	const npmNexusAuth = getInput('nexus-token');
+	const buildNumber = getInput('build-number');
+	const isPR = getInput('is-pr');
+
+	if (npmNexusAuth) {
+		execStep(
+			[
+				`npm config set //repositories.mrf.io/nexus/repository/npm-internal/:_authToken ${npmNexusAuth}`,
+				'npm config set strict-ssl false'
+			],
+			'🔐Config access to marfeel packages...'
+		);
+	}
+
+	if (isPR === 'true') {
+		execStep(
+			[
+				`npx npm-snapshot ${buildNumber} pr-snapshot`,
+				"npm publish --tag canary"
+			],
+			'🚀 Publishing 📦 PR Package 📦...'
+		);
+		return;
+	}
+
+	execStep(
+		[
+			`npx npm-snapshot ${buildNumber} snapshot`,
+			"npm publish --tag latest"
+		],
+		'🚀 Publishing 📦 Package 📦...'
+	);
+
+} catch (error) {
+	setFailed(error.message);
+}
